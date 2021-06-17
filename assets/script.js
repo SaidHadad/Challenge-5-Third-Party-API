fc44f99fbfd0a5127f7a0dcfe7b3cf4ccd8579d0
SaidHadad <saiddavid.hadad@gmail.com>
2021-06-17 12:26:27 -0400

    styling
    Fixed styling on the time and save button columns

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@
 var currentDay = moment().format("[Today is ] dddd, MMMM Do");
 var currentTime = moment().format('hh:mm:ss a');
 
-let amPM = "AM";
 let finalHour = "";
 let timeMap = new Map();
 
@@ -16,19 +15,21 @@ for (let hour = 9; hour < 18; hour++) {
     // 1st column: hour - format time from 24 to 12 HR & create div
 
     let timeDiv = $('<div>');
-    if (hour > 12) {
-        finalHour = ((hour - 12) + ":00 AM");
-    } else {
-        finalHour = (hour + ":00 PM");
+    if (hour > 12 && hour != 12)  {
+        finalHour = ((hour - 12) + " PM");
+    } 
+
+    else if (hour == 12){
+        finalHour = hour + " PM"
     }
 
-    if (finalHour < 10) {
-        // this helps formatting the time column to make them even
-        finalHour = "  " + finalHour;
+    else {
+        finalHour = (hour + " AM");
     }
+
 
     timeDiv.text(finalHour);
-    timeDiv.addClass('hour col-1');
+    timeDiv.addClass('hour col-1 text-center d-flex flex-column justify-content-center');
     
 
     // second column contains the task description
@@ -41,9 +42,9 @@ for (let hour = 9; hour < 18; hour++) {
 
     // third column contains the save button
     var saveIcon = $("<i>");
-    saveIcon.addClass("far fa-save d-flex justify-content-center");
+    saveIcon.addClass("far fa-save");
     var saveDiv = $("<div>");
-    saveDiv.addClass("saveBtn col-1 text-center").attr("id", "button" + hour);
+    saveDiv.addClass("saveBtn col-1 text-center d-flex flex-column justify-content-center").attr("id", "button" + hour);
 
     saveDiv.append(saveIcon);
 
